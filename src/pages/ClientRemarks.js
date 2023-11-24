8c9eaa17b7e53089867a6a98d87791aad87dfbb2
Lawrence Tsungu <tsungulawrence@gmail.com>
2023-11-24 23:16:31 +0300

    code

--- src/pages/ClientRemarks.js
+++ src/pages/ClientRemarks.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import '../styles/client-remarks.css';
 import Testimonial from '../components/Testimonial';
+import Marquee from "react-fast-marquee";
 
 const ClientRemarks = () => {
-  return (
-    <div className='client'>
-        <h2><span className='one'>testimonials</span> <br /> <span className='lxc'>Client Remarks</span></h2>
-        <div className="bottom">
-        <Testimonial />
-        <Testimonial />
-        <Testimonial />
+    return (
+        <div className='client'>
+            <h2><span className='one'>testimonials</span> <br /> <span className='lxc'>Client Remarks</span></h2>
+            <div className="bottom">
+                <Marquee pauseOnHover speed="50">
+                    <Testimonial />
+                    <Testimonial />
+                    <Testimonial />
+                </Marquee>
+
+            </div>
+
         </div>
-       
-    </div>
-  )
+    )
 }
 
 export default ClientRemarks
