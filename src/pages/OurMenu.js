3870af71b21b8a140a361512824faa253abe44db
Lawrence Tsungu <tsungulawrence@gmail.com>
2023-11-25 20:09:59 +0300

    code

--- src/pages/OurMenu.js
+++ src/pages/OurMenu.js
@@ -14,8 +14,8 @@ const OurMenu = () => {
             <div className="menuz">
                 <SpecificMenu price='Ksh.80' image={Fries} food_name="Fries" description="Crunch into perfection with our golden Fries—irresistibly crispy and seasoned to perfection!"/>
                 <SpecificMenu price='Ksh.150' image={Shawarma} food_name="Shawarma" description="Savor the delicious flavor of our Shawarma, a taste sensation that delights every palate."/>
-                <SpecificMenu price='Ksh.250' image={Mkorogo} food_name="Mkorogo" description="Our yummy mkorogo is a perfect reflection of our kitchen artistry."/>
-                <SpecificMenu price='Ksh.150' image={Strawberry}  food_name="Strawberry" description="Taste the pure bliss of our Milkshake with Strawberry."/>  
+                <SpecificMenu price='Ksh.250' image={Mkorogo} food_name="Mkorogo" description="Our yummy Mkorogo showcases kitchen artistry, a perfect gastronomic reflection."/>
+                <SpecificMenu price='Ksh.150' image={Strawberry}  food_name="Strawberry" description="Taste the pure bliss of our Strawberry Milkshake—a delightful symphony of sweetness."/>  
             </div>
             <div className="more">
                 <Button content="More" />
