5e6e15028191de760ce618d875125567e424df58
Lawrence Tsungu <tsungulawrence@gmail.com>
2023-12-08 15:53:56 +0300

    code

--- src/pages/FullMenu.js
+++ src/pages/FullMenu.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import SpecificMenu from '../components/SpecificMenu'
+import Button from '../components/Button'
+import ChipoMasala from '../assets/chipomasala.png'
+import Shawarma from '../assets/shawarma_menu.png'
+import Fries from '../assets/fries-menu.png'
+import Mkorogo from '../assets/mkorogo.png'
+import Strawberry from '../assets/strawberry.png'
 
 const FullMenu = () => {
   return (
-    <div>FullMenu</div>
+    <div className='our-menu'>
+    <h2><span className='one'>Specialties</span><br /><span className='lxc'>Our Menu</span></h2>
+    <div className="menuz">
+        <SpecificMenu price='Ksh.80' image={Fries} food_name="Fries" description="Crunch into perfection with our golden Fries—irresistibly crispy and seasoned to perfection!"/>
+        <SpecificMenu price='Ksh.150' image={Shawarma} food_name="Shawarma" description="Savor the delicious flavor of our Shawarma, a taste sensation that delights every palate."/>
+        <SpecificMenu price='Ksh.250' image={Mkorogo} food_name="Mkorogo" description="Our yummy Mkorogo showcases kitchen artistry, a perfect gastronomic reflection."/>
+        <SpecificMenu price='Ksh.150' image={Strawberry}  food_name="Strawberry" description="Taste the pure bliss of our Strawberry Milkshake—a delightful symphony of sweetness."/>  
+    </div>
+    <div className="more">
+        <Button content="More" />
+    </div>
+</div>
   )
 }
 
