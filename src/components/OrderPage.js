aeef07850edcfac6a4ead4f90f095f04f2ae95b7
Lawrence Tsungu <tsungulawrence@gmail.com>
2023-12-08 20:44:26 +0300

    set payment page

--- src/components/OrderPage.js
+++ src/components/OrderPage.js
@@ -4,13 +4,54 @@ import { FaPlus, FaMinus } from 'react-icons/fa';
 import Button from './Button';
 import { useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
+import { useState } from 'react';
 
 const OrderPage = () => {
+    const [quantity, setQuantity] = useState(1);
+    const [number, setNumber] = useState(1);
+    const [newprice, setNewprice] = useState(1);
+
+
     const location = useLocation();
     const { food_name, price } = location.state || {};
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
+
+    // function minusOrder() {
+    //     if (!number==0) {
+    //         setNumber(() => number - 1);
+    //         setNewprice(()=>number*Number(price.slice(4)));
+    //         setQuantity(()=>quantity-1)
+    //     }
+    // }
+    // function plusOrder() {
+    //     setNumber(() => number + 1);
+    //     setNewprice(()=>number*Number(price.slice(4)));
+    //     setQuantity(()=>quantity+1)
+    // }
+
+
+
+    function minusOrder() {
+        if (number > 0) {
+            setNumber((prevNumber) => prevNumber - 1);
+            setQuantity((prevQuantity) => prevQuantity - 1);
+        }
+    }
+    
+    function plusOrder() {
+        setNumber((prevNumber) => prevNumber + 1);
+        setQuantity((prevQuantity) => prevQuantity + 1);
+    }
+    
+    useEffect(() => {
+        setNewprice(number * Number(price.slice(4)));
+    }, [number, price]);
+
+
+
+    console.log(number,newprice)
     return (
         <div className='orderpage'>
             <div className="name">Shawarma Hub</div>
@@ -18,8 +59,13 @@ const OrderPage = () => {
                 <div className="top">
                     <div className="left">{price}</div>
                     <div className="right">
-                        <span className="minus"><FaMinus/></span>
-                        <span className="plus"><FaPlus/></span>
+                        <span className="minus"
+                            onClick={minusOrder}
+                        ><FaMinus /></span>
+                        <span className='qty'>{quantity}</span>
+                        <span className="plus"
+                            onClick={plusOrder}
+                        ><FaPlus /></span>
                     </div>
                 </div>
                 <div className="middle">
@@ -48,7 +94,7 @@ const OrderPage = () => {
             </div>
             <div className="payment">
                 <h3>Enter Your Phone Number</h3>
-                <input type='number' placeholder='Enter Phone Number (Saf)' />
+                <input type='number' placeholder='Enter Phone Number (Saf)' min="0"/>
                 <div className="pay-btn">
                     {/* <Button content="Pay" /> */}
                     <button className='pay-btn'>Pay</button>
