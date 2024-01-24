da2d5767c040bec1693d5a6869b9e32f43457e40
Lawrence Tsungu <tsungulawrence@gmail.com>
2024-01-24 17:09:06 +0300

    code

--- src/pages/ClientRemarks.js
+++ src/pages/ClientRemarks.js
@@ -9,7 +9,7 @@ const ClientRemarks = () => {
             <h2><span className='one'>Testimonials</span> <br /> <span className='lxc'>Client Remarks</span></h2>
             <div className="bottom">
                 <Marquee pauseOnHover speed="50">
-                    <Testimonial content="Quick bites with a gourmet touch – that's what sets this food joint apart. The delivery is impressively fast, and every item on their menu is a delight. My favorite guilty pleasure, delivered promptly!" author="Lawrence Tsungu"/>
+                    <Testimonial content="Quick bites with a gourmet touch – that's what sets this food joint apart. The delivery is impressively fast, and every item on their menu is a delight. My favorite guilty pleasure, delivered promptly!" author="Lawrence T."/>
                     <Testimonial content="Quick and tasty – that's how I would describe this fast-food joint! The delivery service is prompt, and the quality of the food is consistently top-notch. My go-to spot for a satisfying meal on busy days." author="Ridhwan Soud"/>
                     <Testimonial content="Absolutely love ordering from this food joint! The delivery is always lightning fast, and the food arrives hot and delicious. Their burgers are a game-changer!" author="Kristina Njeri"/>
                
